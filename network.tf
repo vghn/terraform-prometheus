a5474924e11830341bdb76fe25bc0e15471f303a
Vlad <vlad@ghn.me>
2020-05-18 16:06:17 +0000

    Only allow home IPs

--- network.tf
+++ network.tf
@@ -7,6 +7,11 @@ data "aws_subnet" "primary" {
   id = sort(data.aws_subnet_ids.public.ids)[0]
 }
 
+# Allowed IPs
+data "dns_a_record_set" "home" {
+  host = "home.ghn.me"
+}
+
 # Prometheus Instance Security Group
 resource "aws_security_group" "prometheus" {
   name        = "Prometheus"
@@ -22,7 +27,7 @@ resource "aws_security_group_rule" "prometheus_ssh" {
   to_port           = 22
   protocol          = "tcp"
   security_group_id = aws_security_group.prometheus.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = data.dns_a_record_set.home.addrs
 }
 
 resource "aws_security_group_rule" "prometheus_http" {
